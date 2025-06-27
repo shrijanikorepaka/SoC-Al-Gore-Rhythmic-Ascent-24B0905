fb0b188477e8d733038544d8545b2983aebecb46
shrijanikorepaka <shrijanikorepaka@gmail.com>
2025-06-27 22:27:19 +0530

    Create dice_combinations.py

---
+++ Week 5/dice_combinations.py
@@ -0,0 +1,13 @@
+MOD = 10**9 + 7
+
+n = int(input())
+
+dp = [0] * (n + 1)
+dp[0] = 1
+
+for i in range(1, n + 1):
+    for j in range(1, 7):
+        if i - j >= 0:
+            dp[i] = (dp[i] + dp[i - j]) % MOD
+
+print(dp[n])
