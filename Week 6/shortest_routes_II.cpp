a31a17e49b1d2bb755108eac572f99b1e9ddb024
shrijanikorepaka <shrijanikorepaka@gmail.com>
2025-07-10 22:59:46 +0530

    Create shortest_routes_II.cpp

---
+++ Week 6/shortest_routes_II.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const long long INF = 1e15;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    vector<vector<long long>> dist(n + 1, vector<long long>(n + 1, INF));
+
+    for (int i = 1; i <= n; ++i) {
+        dist[i][i] = 0;
+    }
+
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        dist[a][b] = min(dist[a][b], c);
+        dist[b][a] = min(dist[b][a], c);  // undirected road
+    }
+
+    // Floyd-Warshall
+    for (int k = 1; k <= n; ++k) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        if (dist[a][b] == INF) {
+            cout << -1 << "\n";
+        } else {
+            cout << dist[a][b] << "\n";
+        }
+    }
+
+    return 0;
+}
